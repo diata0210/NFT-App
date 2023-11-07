3061ba380fa9fed89baed91d357522ba0a0878f8
Tuan <thaituandang0210@gmail.com>
2023-11-07 22:19:47 +0700

    add 1 file

---
+++ src/main/java/crawler/test.java
@@ -0,0 +1,7 @@
+package crawler;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("abc");
+    }
+}
---
+++ src/main/java/crawler/test1.java
@@ -0,0 +1,7 @@
+package crawler;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
