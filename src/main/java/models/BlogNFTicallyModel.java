1c50dd32bea0520cb8f5da64e0e5b9f297dacb95
diata0210 <128179434+diata0210@users.noreply.github.com>
2023-11-18 22:08:27 +0700

    Feature/oop kien crawl nf tically blog (#11)
    * nifty gateway get api (#4) (#5)
    
    nifty gateway get api
    
    * Rename RaribleAPICall.java to RaribleApiCall.java
    
    * add crawlUpyoBlog
    
    * fix crawlData
    
    * done crawl NFTicallyBlog
    
    * update crawl nfticallyblog
    
    ---------
    
    Co-authored-by: trankien <kien8b2003@gmail.com>

---
+++ src/main/java/models/BlogNFTicallyModel.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BlogNFTicallyModel extends BlogModel implements CustomModel {
+    public BlogNFTicallyModel(String title, String author, String date, List<String> relatedTags) {
+        super.title = title;
+        super.author = author;
+        super.date = date;
+        super.relatedTags = relatedTags;
+    }
+
+    public BlogNFTicallyModel() {
+        super.title = null;
+        super.desc = null;
+        super.author = null;
+        super.date = null;
+        super.relatedTags = null;
+    }
+
+    @Override
+    public Map<String, Object> MapDescription() {
+        Map<String, Object> res = new HashMap<>();
+        res.put("title", title);
+        res.put("author", author);
+        res.put("desc", desc);
+        res.put("relatedTags", relatedTags);
+        res.put("date", date);
+        return res;
+    }
+}
