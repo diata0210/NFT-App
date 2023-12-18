37a2e7d0ce283f6b6669494e020607b1d58317d2
chubebanso <vuquanghoanglam@gmail.com>
2023-12-18 17:47:08 +0700

    search tag

---
+++ src/main/java/service/implement/ImmutableCollectionServiceImp.java
@@ -0,0 +1,30 @@
+package service.implement;
+
+import java.util.List;
+
+import models.ImmutableCollectionModel;
+import repository.ImmutableCollectionRepository;
+import repository.implement.ImmutableCollectionRepositoryImp;
+import service.ImmutableCollectionService;
+
+public class ImmutableCollectionServiceImp implements ImmutableCollectionService {
+    private ImmutableCollectionRepository ImmutableCollectionRepository = ImmutableCollectionRepositoryImp.getInstance();
+
+    public static ImmutableCollectionServiceImp instance;
+
+    public static ImmutableCollectionServiceImp getInstance() {
+        if (instance == null)
+            instance = new ImmutableCollectionServiceImp();
+        return instance;
+    }
+
+    @Override
+    public List<ImmutableCollectionModel> findModelsByName(String name) {
+        return ImmutableCollectionRepository.findModelsByName(name);
+    }
+
+    public List<ImmutableCollectionModel> getAllModels() {
+        return ImmutableCollectionRepository.getAllModels();
+    }
+
+}
