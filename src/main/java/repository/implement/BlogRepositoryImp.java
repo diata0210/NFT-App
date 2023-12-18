197d45526a51b7216dae8738c5073c1cec0fe28d
chubebanso <vuquanghoanglam@gmail.com>
2023-12-19 00:46:41 +0700

    update getTags

--- src/main/java/repository/implement/BlogRepositoryImp.java
+++
@@ -1,9 +0,0 @@
-// package repository.implement;
-
-// import repository.BlogRepository;
-// import repository.Repository;
-
-// public class BlogRepositoryImp extends BlogRepository implements Repository, {
-
-    
-// }
--- src/main/java/service/getAllTags.java
+++ src/main/java/service/getAllTags.java
@@ -6,39 +6,29 @@ import java.util.Map;
 import models.BlogModel;
 
 public class getAllTags {
-    // public static List<String> allTags() {
-    // List<String> allTags = new ArrayList<>();
-    // List<BlogModel> allArticles = getAllArticles.allArticles();
-    // for (BlogModel article : allArticles) {
-    // for (String tags : article.getRelatedTags()) {
-    // if (!allTags.contains(tags)) {
-    // allTags.add(tags);
-    // }
-    // }
-    // }
-    // return allTags;
-    // }
-    public static List<String> getAllTags(String name, String day, String month) {
-        if (day != "" && name == "" && month == "") {
-            return getTagsByDay(day);
-        } else if (month != "" && day == "" && name == "") {
-            return getTagsByMonth(month);
-        } else if (name != "" && day == "" && month == "") {
-            return getAllTagsFilteredByName(name);
-        } else if (name == "" && day == "" && month == "") {
-            return allTags();
-        } else {
-            return null;
+
+    public static List<String> getAllTags(int type, String parameter) {
+        switch (type) {
+            case 0:
+                return allTags();
+            case 1:
+                return getAllTagsFilteredByName(parameter);
+            case 2:
+                return getTagsByDay(parameter);
+            case 3:
+                return getTagsByMonth(parameter);
+            default:
+                return new ArrayList<>();
         }
     }
 
-    public static List<String> allTags() {
+    private static List<String> allTags() {
         List<String> allTags = new ArrayList<>();
         List<BlogModel> allArticles = getAllArticles.allArticles();
         for (BlogModel article : allArticles) {
-            for (String tags : article.getRelatedTags()) {
-                if (!allTags.contains(tags)) {
-                    allTags.add(tags);
+            for (String tag : article.getRelatedTags()) {
+                if (!allTags.contains(tag)) {
+                    allTags.add(tag);
                 }
             }
         }
@@ -48,13 +38,10 @@ public class getAllTags {
     private static List<String> getAllTagsFilteredByName(String name) {
         List<String> allTags = new ArrayList<>();
         List<BlogModel> allArticles = getAllArticles.allArticles();
-
         for (BlogModel article : allArticles) {
             for (String tag : article.getRelatedTags()) {
-                if (name == null || tag.contains(name)) {
-                    if (!allTags.contains(tag)) {
-                        allTags.add(tag);
-                    }
+                if (tag.contains(name) && !allTags.contains(tag)) {
+                    allTags.add(tag);
                 }
             }
         }
@@ -67,31 +54,30 @@ public class getAllTags {
     }
 
     private static List<String> getTagsByMonth(String month) {
-        Map<String, Integer> tagFrequency = getTagFrequencyByMonth.getTagFrequencyByMonth(month);
-        return new ArrayList<>(tagFrequency.keySet());
+        if (month.length() == 2) {
+            Map<String, Integer> tagFrequency = getTagFrequencyByMonth.getTagFrequencyByMonth(month);
+            return new ArrayList<>(tagFrequency.keySet());
+        } else {
+            return null;
+        }
     }
 
     public static void main(String[] args) {
-        // Example usage
-        List<String> tags = getAllTags("NFT", "", ""); // Adjust the parameters as needed
-        for (String tag : tags) {
-            System.out.println(tag);
-        }
-        List<String> tagByDays = getAllTags("", "08-01", "");
-        System.out.println("Hot tags by Day");
-        for (String tag : tagByDays) {
-            System.out.println(tag);
-        }
-        List<String> tagByMonth = getAllTags("", "", "08");
-        System.out.println("Hot tags by Month");
-        for (String tag : tagByMonth) {
-            System.out.println(tag);
-        }
-        System.out.println("All Tag");
-        List<String> allTags = getAllTags("", "", "");
-        for (String tag : allTags) {
-            System.out.println(tag);
-        }
-        System.out.println(getAllTags("NFT", null, "09"));
+        List<String> allTags = getAllTags(0, null);
+        List<String> tagsByName = getAllTags(1, "NFT");
+        List<String> tagsByDay = getAllTags(2, "08-01");
+        List<String> tagsByMonth = getAllTags(3, "08");
+
+        System.out.println("All Tags:");
+        allTags.forEach(System.out::println);
+
+        System.out.println("\nTags by Name:");
+        tagsByName.forEach(System.out::println);
+
+        System.out.println("\nTags by Day:");
+        tagsByDay.forEach(System.out::println);
+
+        System.out.println("\nTags by Month:");
+        tagsByMonth.forEach(System.out::println);
     }
 }
