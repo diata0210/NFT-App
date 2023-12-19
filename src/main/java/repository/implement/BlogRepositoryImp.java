dfeae5fb98e3d50c2ee1f8d4438e2ef955d30ed8
Tuan <thaituandang0210@gmail.com>
2023-12-20 00:26:49 +0700

    Revert "Search tags (#34)"
    This reverts commit decfc6060bd4b40e3be915006334e95b910a42e8.

---
+++ src/main/java/repository/implement/BlogRepositoryImp.java
@@ -0,0 +1,9 @@
+// package repository.implement;
+
+// import repository.BlogRepository;
+// import repository.Repository;
+
+// public class BlogRepositoryImp extends BlogRepository implements Repository, {
+
+    
+// }
--- src/main/java/service/getAllTags.java
+++ src/main/java/service/getAllTags.java
@@ -2,62 +2,26 @@ package service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import models.BlogModel;
 
 public class getAllTags {
-    public static List<String> getAllTags(int type, String parameter) {
-        switch (type) {
-            case 0:
-                return allTags();
-            case 1:
-                return getAllTagsFilteredByName(parameter);
-            case 2:
-                return getTagsByDay(parameter);
-            case 3:
-                return getTagsByMonth(parameter);
-            default:
-                return new ArrayList<>();
-        }
-    }
 
-    private static List<String> allTags() {
+    public static List<String> allTags() {
         List<String> allTags = new ArrayList<>();
         List<BlogModel> allArticles = getAllArticles.allArticles();
         for (BlogModel article : allArticles) {
-            for (String tag : article.getRelatedTags()) {
-                if (!allTags.contains(tag)) {
-                    allTags.add(tag);
+            for (String tags : article.getRelatedTags()) {
+                if (!allTags.contains(tags)) {
+                    allTags.add(tags);
                 }
             }
         }
         return allTags;
     }
-
-    private static List<String> getAllTagsFilteredByName(String name) {
-        List<String> allTags = new ArrayList<>();
-        List<BlogModel> allArticles = getAllArticles.allArticles();
-        for (BlogModel article : allArticles) {
-            for (String tag : article.getRelatedTags()) {
-                if (tag.contains(name) && !allTags.contains(tag)) {
-                    allTags.add(tag);
-                }
-            }
+    public static void main(String[] args) {
+        List<String> allTags=allTags();
+        for (String tags:allTags){
+            System.out.println(tags);
         }
-        return allTags;
-    }
-
-    private static List<String> getTagsByDay(String day) {
-        Map<String, Integer> tagFrequency = getTagFrequencyByDay.getTagFrequencyByDay(day);
-        return new ArrayList<>(tagFrequency.keySet());
-    }
-
-    private static List<String> getTagsByMonth(String month) {
-        if (month.length() == 2) {
-            Map<String, Integer> tagFrequency = getTagFrequencyByMonth.getTagFrequencyByMonth(month);
-            return new ArrayList<>(tagFrequency.keySet());
-        } else {
-            return null;
-        }
-    }
+}
 }
