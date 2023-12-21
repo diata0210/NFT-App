814866fec3203d6e8cd8957522aa8e3960d5b830
Bảo Đỗ <109888400+kuokubao@users.noreply.github.com>
2023-12-21 12:25:56 +0700

    gop commit

---
+++ src/main/java/controller/TrendingController.java
@@ -0,0 +1,59 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.NFTData;
+import service.TrendingNFTService;
+import models.ApiModel;
+
+import java.util.Map;
+
+public class TrendingController {
+
+    @FXML
+    private TableColumn<NFTData, Double> floorPrice;
+
+    @FXML
+    private TableColumn<NFTData, String> name;
+
+    @FXML
+    private TableView<NFTData> tableTrend;
+
+    @FXML
+    private TableColumn<NFTData, Integer> tagCount;
+
+    private TrendingNFTService trendingService = new TrendingNFTService();
+
+    @FXML
+    public void initialize() {
+        initializeColumns(); // Khởi tạo cấu trúc cột
+        updateTableData();
+    }
+
+    private void updateTableData() {
+        Map<ApiModel, Integer> trendingNFTsWithTagCount = trendingService.getTrendingNFTsWithTagCount();
+
+        tableTrend.getItems().clear(); // Xóa dữ liệu cũ trước khi cập nhật mới
+
+        for (Map.Entry<ApiModel, Integer> entry : trendingNFTsWithTagCount.entrySet()) {
+            ApiModel apiModel = entry.getKey();
+            Integer tagCount = entry.getValue();
+
+            NFTData nftData = new NFTData(
+                    apiModel.getName(),
+                    apiModel.getFloorPrice(),
+                    tagCount
+            );
+
+            tableTrend.getItems().add(nftData); // Thêm dữ liệu mới vào bảng
+        }
+    }
+    private void initializeColumns() {
+        // Kết nối cột với thuộc tính trong NFTData
+        floorPrice.setCellValueFactory(new PropertyValueFactory<>("floorPrice"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tagCount.setCellValueFactory(new PropertyValueFactory<>("tagCount"));
+    }
+}
