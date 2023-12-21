5d9c15b398f8702239a59f5894b54d2c4a221937
Kiều Linh <118679986+sansanhatecode@users.noreply.github.com>
2023-12-21 20:58:57 +0700

    adjust datePicker

--- src/main/java/controller/TagsController.java
+++ src/main/java/controller/TagsController.java
@@ -16,101 +16,115 @@ import javafx.scene.layout.VBox;
 import models.TagTableType;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 public class TagsController implements Initializable {
-    private String searchNameString;
-    private int type = 0;
-    private String date;
+  private String searchNameString;
+  private int type = 0;
+  private String date;
 
-    @FXML
-    public TableView<TagTableType> table;
+  @FXML
+  public TableView<TagTableType> table;
 
-    @FXML
-    public TableColumn<TagTableType, Integer> id;
+  @FXML
+  public TableColumn<TagTableType, Integer> id;
 
-    @FXML
-    public TableColumn<TagTableType, String> tag;
+  @FXML
+  public TableColumn<TagTableType, String> tag;
 
-    @FXML
-    public VBox wrapTable;
-    public ObservableList<TagTableType> list;
+  @FXML
+  public VBox wrapTable;
+  public ObservableList<TagTableType> list;
 
-    @FXML
-    public TextField searchByName;
+  @FXML
+  public TextField searchByName;
 
-    @FXML
-    void onSearchByName(ActionEvent event) {
-        searchNameString = searchByName.getText();
-        System.out.println(searchNameString);
+  @FXML
+  public ComboBox<String> filterType;
+
+  @FXML
+  private DatePicker datePicker;
+
+  @FXML
+  void onSearchByName(ActionEvent event) {
+    searchNameString = searchByName.getText();
+    System.out.println(searchNameString);
+  }
+
+  @FXML
+  void onFilterType(ActionEvent event) {
+    String typeValue = filterType.getSelectionModel().getSelectedItem().toString();
+    if (typeValue.equals("Customize")) {
+      type = 0;
+    } else if (typeValue.equals("Filter by date")) {
+      type = 1;
+    } else if (typeValue.equals("Filter by week")) {
+      type = 2;
+    } else if (typeValue.equals("Filter by month")) {
+      type = 3;
+    }
+    changeDatePickerVisibility(type);
+  }
+
+  @FXML
+  void onDateChange(ActionEvent event) {
+    date = datePicker.getValue().toString();
+    System.out.println(date);
+  }
+
+  @Override
+  public void initialize(URL arg0, ResourceBundle arg1) {
+    initializeFilterType();
+    initializeTable();
+    initializeColumns();
+  }
+
+  private void initializeFilterType() {
+    ObservableList<String> selectList = FXCollections.observableArrayList("Customize", "Filter by date",
+        "Filter by week", "Filter by month");
+    filterType.setItems(selectList);
+  }
+
+  private void initializeColumns() {
+    id.setCellValueFactory(new PropertyValueFactory<>("id"));
+    tag.setCellValueFactory(new PropertyValueFactory<>("tag"));
+  }
+
+  private void initializeTable() {
+    list = FXCollections.observableArrayList();
+    List<String> tags = new ArrayList<>();
+    // tags = getAllTags.allTags();
+    int idx = 0;
+    for (String tag : tags) {
+      idx += 1;
+      TagTableType newtag = new TagTableType(idx, tag);
+      list.add(newtag);
     }
 
-    @FXML
-    public ComboBox<String> filterType;
+    table.setRowFactory(tv -> {
+      TableRow<TagTableType> row = new TableRow<>();
+      row.setOnMouseClicked(event -> {
+        TagTableType clickedTag = row.getItem();
+        System.out.println(clickedTag.tag);
+      });
+      return row;
+    });
 
-    @FXML
-    void onFilterType(ActionEvent event) {
-        String typeValue = filterType.getSelectionModel().getSelectedItem().toString();
-        if (typeValue.equals("Customize")) {
-            type = 0;
-        } else if (typeValue.equals("Filter by date")) {
-            type = 1;
-        } else if (typeValue.equals("Filter by week")) {
-            type = 2;
-        } else if (typeValue.equals("Filter by month")) {
-            type = 3;
-        }
-    }
+    table.setItems(list);
 
-    @FXML
-    private DatePicker datePicker;
+    initializeColumns();
+    datePicker.setVisible(false);
+    datePicker.setValue(LocalDate.now());
+  }
 
-    @FXML
-    void onDateChange(ActionEvent event) {
-        date = datePicker.getValue().toString();
-        System.out.println(date);
-    }
-
-    @Override
-    public void initialize(URL arg0, ResourceBundle arg1) {
-        initializeFilterType();
-        initializeTable();
-        initializeColumns();
-    }
-
-    private void initializeFilterType() {
-        ObservableList<String> selectList = FXCollections.observableArrayList("Customize", "Filter by date",
-                "Filter by week", "Filter by month");
-        filterType.setItems(selectList);
-    }
-
-        private void initializeColumns() {
-        id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        tag.setCellValueFactory(new PropertyValueFactory<>("tag"));
-    }
-
-    private void initializeTable() {
-        list = FXCollections.observableArrayList();
-        List<String> tags = new ArrayList<>();
-        // tags = getAllTags.allTags();
-        int idx = 0;
-        for (String tag : tags) {
-            idx += 1;
-            TagTableType newtag = new TagTableType(idx, tag);
-            list.add(newtag);
-        }
-
-        table.setRowFactory(tv -> {
-            TableRow<TagTableType> row = new TableRow<>();
-            row.setOnMouseClicked(event -> {
-                TagTableType clickedTag = row.getItem();
-                System.out.println(clickedTag.tag);
-            });
-            return row;
-        });
-
-        table.setItems(list);
-    }
+  @FXML
+  void changeDatePickerVisibility(int type) {
+    if (type == 0) {
+      datePicker.setVisible(false);
+    } else
+      datePicker.setVisible(true);
+  }
 }
