69a2c2ef5b9d8377ef85fa201c7241faa6d78068
phamxuanphong1307 <117462104+phamxuanphong1307@users.noreply.github.com>
2023-12-24 17:54:35 +0700

    format code

--- src/main/java/controller/BlogsTableController.java
+++ src/main/java/controller/BlogsTableController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.List;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -11,18 +12,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-<<<<<<< HEAD
-=======
-
-import models.Blog;
->>>>>>> 40ab100 (format code)
 import models.BlogModel;
 import models.TagTableType;
 import models.BlogTableType;
 import service.ArticleSearchService;
 
 public class BlogsTableController implements Initializable {
-<<<<<<< HEAD
     @FXML
     private TableView<BlogTableType> table;
     @FXML
@@ -40,30 +35,12 @@ public class BlogsTableController implements Initializable {
     private ResourceBundle resources;
     @FXML
     private URL location;
-=======
-  @FXML
-  private TableView<Blog> table;
-  @FXML
-  private TableColumn<Blog, String> titleCol;
-  @FXML
-  private TableColumn<Blog, String> desCol;
-  @FXML
-  private TableColumn<Blog, String> authorCol;
-  @FXML
-  private TableColumn<Blog, String> dateCol;
-  @FXML
-  private TableColumn<Blog, String> relatedTagsCol;
->>>>>>> 40ab100 (format code)
 
-  @FXML
-  private ResourceBundle resources;
-  @FXML
-  private URL location;
+    @FXML
+    private Label tagNameLabel;
 
-  @FXML
-  private Label tagNameLabel;
+    private TagTableType tag;
 
-<<<<<<< HEAD
     private ObservableList<BlogTableType> blogList;
 
     public void setTable(TagTableType tag) {
@@ -74,17 +51,13 @@ public class BlogsTableController implements Initializable {
         List<String> relatedTagsList;
         List<BlogModel> returnedList = ArticleSearchService.searchArticlesByTag(this.tag.getTag());
         blogList = FXCollections.observableArrayList();
-=======
-  private TagTableType tag;
 
-  private ObservableList<Blog> blogList;
->>>>>>> 40ab100 (format code)
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        desCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
+        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+        relatedTagsCol.setCellValueFactory(new PropertyValueFactory<>("relatedTags"));
 
-  public void setTable(TagTableType tag) {
-    this.tag = tag;
-    tagNameLabel.setText("#" + this.tag.getTag());
-
-<<<<<<< HEAD
         for (BlogModel blog : returnedList) {
             title = blog.getTitle();
             desc = blog.getDesc();
@@ -104,42 +77,10 @@ public class BlogsTableController implements Initializable {
             blogList.add(newBlog);
         }
         table.setItems(blogList);
-=======
-    String title, desc, author, date, relatedTags;
-    List<String> relatedTagsList;
-    List<BlogModel> returnedList = ArticleSearchService.searchArticlesByTag(this.tag.getTag());
-    blogList = FXCollections.observableArrayList();
-
-    titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
-    desCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
-    authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
-    dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
-    relatedTagsCol.setCellValueFactory(new PropertyValueFactory<>("relatedTags"));
-
-    for (BlogModel blog : returnedList) {
-      title = blog.getTitle();
-      desc = blog.getDesc();
-      author = blog.getAuthor();
-      date = blog.getDate();
-      relatedTagsList = blog.getRelatedTags();
-      relatedTags = "";
-      for (String relatedTag : relatedTagsList) {
-        relatedTags = relatedTag + ", " + relatedTags;
-      }
-      Blog newBlog = new Blog();
-      newBlog.setTitle(title);
-      newBlog.setDesc(desc);
-      newBlog.setAuthor(author);
-      newBlog.setDate(date);
-      newBlog.setRelatedTags(relatedTags);
-      blogList.add(newBlog);
->>>>>>> 40ab100 (format code)
     }
-    table.setItems(blogList);
-  }
 
-  @Override
-  public void initialize(URL location, ResourceBundle resources) {
-  }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+    }
 
 }
