2cea59c7080cfee2576a523582bc795bf912b1c9
phamxuanphong1307 <117462104+phamxuanphong1307@users.noreply.github.com>
2023-12-24 17:54:35 +0700

    format code

--- src/main/java/controller/BlogsTableController.java
+++ src/main/java/controller/BlogsTableController.java
@@ -3,7 +3,6 @@ package controller;
 import java.util.List;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,12 +11,18 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+<<<<<<< HEAD
+=======
+
+import models.Blog;
+>>>>>>> 40ab100 (format code)
 import models.BlogModel;
 import models.TagTableType;
 import models.BlogTableType;
 import service.ArticleSearchService;
 
 public class BlogsTableController implements Initializable {
+<<<<<<< HEAD
     @FXML
     private TableView<BlogTableType> table;
     @FXML
@@ -35,12 +40,30 @@ public class BlogsTableController implements Initializable {
     private ResourceBundle resources;
     @FXML
     private URL location;
+=======
+  @FXML
+  private TableView<Blog> table;
+  @FXML
+  private TableColumn<Blog, String> titleCol;
+  @FXML
+  private TableColumn<Blog, String> desCol;
+  @FXML
+  private TableColumn<Blog, String> authorCol;
+  @FXML
+  private TableColumn<Blog, String> dateCol;
+  @FXML
+  private TableColumn<Blog, String> relatedTagsCol;
+>>>>>>> 40ab100 (format code)
 
-    @FXML
-    private Label tagNameLabel;
+  @FXML
+  private ResourceBundle resources;
+  @FXML
+  private URL location;
 
-    private TagTableType tag;
+  @FXML
+  private Label tagNameLabel;
 
+<<<<<<< HEAD
     private ObservableList<BlogTableType> blogList;
 
     public void setTable(TagTableType tag) {
@@ -51,13 +74,17 @@ public class BlogsTableController implements Initializable {
         List<String> relatedTagsList;
         List<BlogModel> returnedList = ArticleSearchService.searchArticlesByTag(this.tag.getTag());
         blogList = FXCollections.observableArrayList();
+=======
+  private TagTableType tag;
 
-        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
-        desCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
-        authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
-        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
-        relatedTagsCol.setCellValueFactory(new PropertyValueFactory<>("relatedTags"));
+  private ObservableList<Blog> blogList;
+>>>>>>> 40ab100 (format code)
 
+  public void setTable(TagTableType tag) {
+    this.tag = tag;
+    tagNameLabel.setText("#" + this.tag.getTag());
+
+<<<<<<< HEAD
         for (BlogModel blog : returnedList) {
             title = blog.getTitle();
             desc = blog.getDesc();
@@ -77,10 +104,42 @@ public class BlogsTableController implements Initializable {
             blogList.add(newBlog);
         }
         table.setItems(blogList);
-    }
+=======
+    String title, desc, author, date, relatedTags;
+    List<String> relatedTagsList;
+    List<BlogModel> returnedList = ArticleSearchService.searchArticlesByTag(this.tag.getTag());
+    blogList = FXCollections.observableArrayList();
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+    desCol.setCellValueFactory(new PropertyValueFactory<>("desc"));
+    authorCol.setCellValueFactory(new PropertyValueFactory<>("author"));
+    dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+    relatedTagsCol.setCellValueFactory(new PropertyValueFactory<>("relatedTags"));
+
+    for (BlogModel blog : returnedList) {
+      title = blog.getTitle();
+      desc = blog.getDesc();
+      author = blog.getAuthor();
+      date = blog.getDate();
+      relatedTagsList = blog.getRelatedTags();
+      relatedTags = "";
+      for (String relatedTag : relatedTagsList) {
+        relatedTags = relatedTag + ", " + relatedTags;
+      }
+      Blog newBlog = new Blog();
+      newBlog.setTitle(title);
+      newBlog.setDesc(desc);
+      newBlog.setAuthor(author);
+      newBlog.setDate(date);
+      newBlog.setRelatedTags(relatedTags);
+      blogList.add(newBlog);
+>>>>>>> 40ab100 (format code)
     }
+    table.setItems(blogList);
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+  }
 
 }
--- src/main/java/controller/TrendingController.java
+++ src/main/java/controller/TrendingController.java
@@ -12,52 +12,49 @@ import java.util.Map;
 
 public class TrendingController {
 
-    @FXML
-    private TableColumn<NFTData, Double> floorPrice;
+  @FXML
+  private TableColumn<NFTData, Double> floorPrice;
 
-    @FXML
-    private TableColumn<NFTData, String> name;
+  @FXML
+  private TableColumn<NFTData, String> name;
 
-    @FXML
-    private TableView<NFTData> tableTrend;
+  @FXML
+  private TableView<NFTData> tableTrend;
 
-    @FXML
-    private TableColumn<NFTData, Integer> tagCount;
+  @FXML
+  private TableColumn<NFTData, Integer> tagCount;
 
-    private TrendingNFTService trendingService = new TrendingNFTService();
+  private TrendingNFTService trendingService = new TrendingNFTService();
 
-    @FXML
-    public void initialize() {
-        initializeColumns(); // Khởi tạo cấu trúc cột
-        updateTableData();
+  @FXML
+  public void initialize() {
+    initializeColumns(); // Khởi tạo cấu trúc cột
+    updateTableData();
+  }
+
+  private void initializeColumns() {
+    // Kết nối cột với thuộc tính trong NFTData
+    floorPrice.setCellValueFactory(new PropertyValueFactory<>("floorPrice"));
+    name.setCellValueFactory(new PropertyValueFactory<>("name"));
+    tagCount.setCellValueFactory(new PropertyValueFactory<>("tagCount"));
+  }
+
+  private void updateTableData() {
+    Map<ApiModel, Integer> trendingNFTsWithTagCount = trendingService.getTrendingNFTsWithTagCount();
+
+    tableTrend.getItems().clear(); // Xóa dữ liệu cũ trước khi cập nhật mới
+
+    for (Map.Entry<ApiModel, Integer> entry : trendingNFTsWithTagCount.entrySet()) {
+      ApiModel apiModel = entry.getKey();
+      Integer tagCount = entry.getValue();
+
+      NFTData nftData = new NFTData(
+          apiModel.getName(),
+          apiModel.getFloorPrice(),
+          tagCount);
+
+      tableTrend.getItems().add(nftData); // Thêm dữ liệu mới vào bảng
     }
-
-
-    private void initializeColumns() {
-        // Kết nối cột với thuộc tính trong NFTData
-        floorPrice.setCellValueFactory(new PropertyValueFactory<>("floorPrice"));
-        name.setCellValueFactory(new PropertyValueFactory<>("name"));
-        tagCount.setCellValueFactory(new PropertyValueFactory<>("tagCount"));
-    }
-
-
-    private void updateTableData() {
-        Map<ApiModel, Integer> trendingNFTsWithTagCount = trendingService.getTrendingNFTsWithTagCount();
-
-        tableTrend.getItems().clear(); // Xóa dữ liệu cũ trước khi cập nhật mới
-
-        for (Map.Entry<ApiModel, Integer> entry : trendingNFTsWithTagCount.entrySet()) {
-            ApiModel apiModel = entry.getKey();
-            Integer tagCount = entry.getValue();
-
-            NFTData nftData = new NFTData(
-                    apiModel.getName(),
-                    apiModel.getFloorPrice(),
-                    tagCount
-            );
-
-            tableTrend.getItems().add(nftData); // Thêm dữ liệu mới vào bảng
-        }
-    }
+  }
 
 }
